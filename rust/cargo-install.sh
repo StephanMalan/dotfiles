4aff714baff618404ddddf2b31501440f6bc1a4d
Stephan Malan <stephanmalan.rob@gmail.com>
2023-07-17 18:02:38 +0200

    Update cargo install to be universal

---
+++ rust/cargo-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Cargo install/update crates
+
+source "$DOTFILES/install/utils.sh"
+
+cargo_install() {
+    info 0 "Installing/Updating '$1' crate"
+    cargo install -q $1
+    success 1 "Installed/Updated '$1'"
+}
+
+info 0 "Installing/Updating cargo crates:"
+
+cat "$DOTFILES/rust/cargo.list" | while IFS= read -r line; do
+    info 1 "Installing/Updating '$line' crate"
+    cargo install -q $line
+done
+
+success 1 "Installed/Updated cargo crates."
--- rust/install.ubuntu.sh
+++ rust/install.ubuntu.sh
@@ -13,21 +13,4 @@ else
     success 1 "Installed Rust."
 fi
 
-echo ""
-info 0 "Installing cargo crates:"
-
-cargo install -q bat | pipe_output 1
-cargo install -q fd-find | pipe_output 1
-cargo install -q tokei | pipe_output 1
-cargo install -q speedtest-rs | pipe_output 1
-cargo install -q cargo-show-asm | pipe_output 1
-cargo install -q cargo-expand | pipe_output 1
-cargo install -q irust | pipe_output 1
-cargo install -q gitui | pipe_output 1
-cargo install -q ripgrep | pipe_output 1
-cargo install -q exa | pipe_output 1
-cargo install -q zellij | pipe_output 1
-cargo install -q bottom | pipe_output 1
-cargo install -q lsd | pipe_output 1
-
-success 1 "Installed cargo crates."
+source "$DOTFILES/rust/cargo-install.sh"
