88464f09c70cb524aa7f3a0c00835a2dfa960045
Stephan Malan <stephanmalan.rob@gmail.com>
2023-07-17 18:04:25 +0200

    Update script

--- install/main-install.ubuntu.sh
+++ install/main-install.ubuntu.sh
@@ -29,6 +29,10 @@ sudo apt-get install \
     zsh \
     -y -q | pipe_output 1
 sudo snap install docker | pipe_output 1
+sudo snap install dbeaver-ce | pipe_output 1
+sudo snap install discord | pipe_output 1
+sudo snap install postman | pipe_output 1
+sudo snap install intellij-idea-ultimate --classic --edge
 sudo setfacl --modify user:${USER}:rw /var/run/docker.sock
 sudo snap install --classic code | pipe_output 1
 sudo snap install --edge nvim --classic | pipe_output 1
